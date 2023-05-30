0e8431435c3b1da26738f64bcadea40e9df0ef6d
Andy Rog <andy.rog@t-online.de>
2023-05-30 21:03:04 +0200

    In der Projektmape neu  Ordner Tests mit  den  xUnit Test Projekten: Bookstore.Domain.Unittests und Bookstore.Applikation.Unittests und entsprechenden Projektverweisen eerstellt.

---
+++ Bookstore.Application.Unittests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Bookstore.Application.Unittests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Bookstore.Domain.Unittests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Bookstore.Domain.Unittests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
