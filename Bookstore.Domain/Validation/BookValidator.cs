8fe54d7556ddbf15d560455d9f69670819c8ab56
Andy Rog <andy.rog@t-online.de>
2023-05-30 22:01:41 +0200

    Logik (die Regeln) für der Klasse BookValidator implementieren  Book.Isbn sollte Länge 13 haben. BookQuantity sollte größer, >0 oder gleich = 0 sein. Book.Author sollte nicht Null sein.

--- Bookstore.Domain/Validation/BookValidator.cs
+++ Bookstore.Domain/Validation/BookValidator.cs
@@ -6,9 +6,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bookstore.Domain.Validation
+namespace Bookstore.Domain.Validation;
+
+public class BookValidator : AbstractValidator<Book>
 {
-    public class BookValidator : AbstractValidator<Book>
+    public BookValidator() 
     {
-    }
+        RuleFor(book => book.Isbn).Length(13);
+        RuleFor(book => book.Quantity).GreaterThanOrEqualTo(0);
+        RuleFor(book => book.Author).NotNull();
+    }  
 }
