2aa8d2d92aeb986e69d2f4f291e3f0572b305cb3
Andy Rog <andy.rog@t-online.de>
2023-05-30 22:18:15 +0200

    Ein neue Author Test-Objekt erstellt.

--- Bookstore.Domain.Unittests/Validation/BookValidatorTests.cs
+++ Bookstore.Domain.Unittests/Validation/BookValidatorTests.cs
@@ -4,7 +4,12 @@ namespace Bookstore.Domain.Unittests.Validation
 {
     public class BookValidatorTests
     {
-        
+        private Author Author { get; } = new Author()
+        {
+            Id = 1,
+            FirstName = "Test",
+            LastName = "Test"
+        };
 
         [Fact]
         public void Test1()
