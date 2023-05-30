77c88d95ab726eb46f15cd56bdffd5dc898be91c
Andy Rog <andy.rog@t-online.de>
2023-05-30 22:16:25 +0200

    Mit NuGet-Paket-Manager eine Validierungsbibliothek für .NET, die eine Fluent-Schnittstelle zum Erstellen stark typisierter Validierungsregeln (FluentValidator) installieren.

--- Bookstore.Domain.Unittests/Validation/BookValidatorTests.cs
+++ Bookstore.Domain.Unittests/Validation/BookValidatorTests.cs
@@ -1,7 +1,11 @@
+using Bookstore.Domain.Entities;
+
 namespace Bookstore.Domain.Unittests.Validation
 {
     public class BookValidatorTests
     {
+        
+
         [Fact]
         public void Test1()
         {
