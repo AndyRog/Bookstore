26e6a757e03177c5298fc8bfe42e94966e625253
Andy Rog <andy.rog@t-online.de>
2023-05-31 19:54:08 +0200

    #Mapping_Configuration_Is_Valid()

---
+++ Bookstore.Application.Unittests/DtoEntityMapperProfileTests.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Xunit;
+
+namespace Bookstore.Application.Unittests;
+
+public class DtoEntityMapperProfileTests 
+{
+    [Fact]
+    public void Mapping_Configuration_Is_Valid()
+    {
+        var config = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile)));
+
+        //Assert
+
+        config.AssertConfigurationIsValid();
+        
+    }
+}
