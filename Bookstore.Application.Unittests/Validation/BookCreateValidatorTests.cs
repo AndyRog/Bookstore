504f5f320bc32017255bc505cb66d99f26f66eda
Andy Rog <andy.rog@t-online.de>
2023-05-31 14:20:04 +0200

    Arrenge-Block: neue Objekt erstellt Act-Block: das Objeckt in Validate(bookCreate)  übergeben Assert-Block: Ergebnis result.True(result.IsValid) geprüft.

--- Bookstore.Application.Unittests/Validation/BookCreateValidatorTests.cs
+++ Bookstore.Application.Unittests/Validation/BookCreateValidatorTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bookstore.Application.Dtos;
+using Bookstore.Application.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace Bookstore.Application.Unittests.Validation
 {
-    internal class BookCreateValidatorTests
+    public class BookCreateValidatorTests
     {
+        private BookCreateValidator BookCreateValidator { get; } = new BookCreateValidator();
+
+        [Fact]
+        public void Valid_BookCreate_Passes_Validation()
+        {
+            //Arrange
+            var bookCreate = new BookCreate("1234567891234", "Test", 1, 0);
+
+            //Act
+            var result = BookCreateValidator.Validate(bookCreate);
+
+            //Assert
+            Assert.True(result.IsValid);
+        
+        }
     }
 }
