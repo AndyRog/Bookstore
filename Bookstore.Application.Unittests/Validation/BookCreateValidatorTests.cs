e142ea0ede031f5d90528832fd0c78d008724b94
Andy Rog <andy.rog@t-online.de>
2023-05-31 14:29:45 +0200

    Neue Test-Method:  Validation_Error_For_Emty_Titel() Arrange-Block:  Titel = string.Empty; Assert-Block:  Assert.False(result.IsValid); und auf bestimte Validation-Fehler prüfen mit Validator: "NotEmpyValidator" und PropertyName "Titel"

--- Bookstore.Application.Unittests/Validation/BookCreateValidatorTests.cs
+++ Bookstore.Application.Unittests/Validation/BookCreateValidatorTests.cs
@@ -25,5 +25,21 @@ namespace Bookstore.Application.Unittests.Validation
             Assert.True(result.IsValid);
         
         }
+        
+        [Fact]
+        public void Validation_Error_For_Emty_Titel()
+        {
+            //Arrange
+            var bookCreate = new BookCreate("1234567891234", string.Empty, 1, 0);
+
+            //Act
+            var result = BookCreateValidator.Validate(bookCreate);
+
+            //Assert
+            Assert.False(result.IsValid);
+            Assert.Single(result.Errors);
+            Assert.Contains(result.Errors, error=>error.ErrorCode.Equals("NotEmpyValidator") && error.PropertyName.Equals("Titel"));
+        
+        }
     }
 }
