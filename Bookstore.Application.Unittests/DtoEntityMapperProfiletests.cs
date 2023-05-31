5a6af3301777349becd6b844b01f966dc68055e4
Andy Rog <andy.rog@t-online.de>
2023-05-31 11:41:10 +0200

    Neue Tests- Klasse DtoEntityMapperProfileTests mit TEst-Method Mapping_Configuration_Is_Valid() erstellt

---
+++ Bookstore.Application.Unittests/DtoEntityMapperProfiletests.cs
@@ -0,0 +1,11 @@
+namespace Bookstore.Application.Unittests
+{
+    public class DtoEntityMapperProfileTests 
+    {
+        [Fact]
+        public void Mapping_Configuration_Is_Valid()
+        {
+
+        }
+    }
+}
