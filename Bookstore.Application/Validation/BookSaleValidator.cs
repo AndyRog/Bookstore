8b33b75d62fa3c01847888de9d660e28674ac7ab
Andy Rog <andy.rog@t-online.de>
2023-06-01 11:35:06 +0200

    Neue class BookSaleValidator : AbstractValidator<BookSale> Mit Regel: RuleFor(sale => sale.Quantity).GreaterThan(0);

---
+++ Bookstore.Application/Validation/BookSaleValidator.cs
@@ -0,0 +1,17 @@
+﻿using Bookstore.Application.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Validation;
+
+public class BookSaleValidator : AbstractValidator<BookDelivery>
+{
+    public BookSaleValidator()
+    {
+        RuleFor(sale => sale.Quantity).GreaterThan(0);
+    }
+}
