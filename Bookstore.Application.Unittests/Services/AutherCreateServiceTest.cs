43bab4f4e4bdbc1e90277a14d7a80d3a1a0b47c9
Andy Rog <andy.rog@t-online.de>
2023-06-01 14:52:27 +0200

    Test: Create_Author mit Mock für IAuthorRepository

--- Bookstore.Application.Unittests/Services/AutherCreateServiceTest.cs
+++ Bookstore.Application.Unittests/Services/AutherCreateServiceTest.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using AutoMapper;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Validation;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Bookstore.Application.Unittests.Services
 {
-    internal class AutherCreateServiceTest
+    public class AutherCreateServiceTest
     {
+        private IMapper Mapper { get; }
+        private AuthorCreateValidator Validator { get; }
+
+        public AutherCreateServiceTest()
+        {
+            Mapper = new MapperConfiguration(config => config.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+
+            Validator = new AuthorCreateValidator();
+        }
+
+        [Fact]
+        public async Task Create_Author()
+        {
+            //Arrange
+            var authorCreate = new AuthorCreate("test", "test");
+
+            var autherRepositoryMock = new Mock<>
+        }
+
     }
 }
