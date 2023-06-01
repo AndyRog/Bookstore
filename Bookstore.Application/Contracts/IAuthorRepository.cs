919a0aa524b052834aecf93ebd9fedcef42c1cd7
Andy Rog <andy.rog@t-online.de>
2023-06-01 14:32:50 +0200

    In dem Method: public async Task<long> CreateAuthor(AuthorCreate authorCreate) Validierung und Mapper erstellen

--- Bookstore.Application/Contracts/IAuthorRepository.cs
+++ Bookstore.Application/Contracts/IAuthorRepository.cs
@@ -1,5 +1,8 @@
-﻿namespace Bookstore.Application.Contracts;
+﻿using Bookstore.Application.Dtos;
+
+namespace Bookstore.Application.Contracts;
 
 public interface IAuthorRepository
 {
+    Task<long> AddAuthor(AuthorCreate author);
 }
--- Bookstore.Application/Services/AuthorCreateService.cs
+++ Bookstore.Application/Services/AuthorCreateService.cs
@@ -2,6 +2,7 @@
 using Bookstore.Application.Contracts;
 using Bookstore.Application.Dtos;
 using Bookstore.Application.Validation;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +14,19 @@ namespace Bookstore.Application.Services;
 public class AuthorCreateService
 {
     public IAuthorRepository AuthorRepository { get; }
-    
+    public IMapper Mapper { get; }
+    public AuthorCreateValidator AuthorCreateValidator { get; }
+
     public AuthorCreateService(IAuthorRepository authorRepository, IMapper mapper, AuthorCreateValidator authorCreateValidator)
     {
         AuthorRepository = authorRepository;
+        Mapper = mapper;
+        AuthorCreateValidator = authorCreateValidator;
     }
     public async Task<long> CreateAuthor(AuthorCreate authorCreate)
     {
-
+        await AuthorCreateValidator.ValidateAndThrowAsync(authorCreate);
+        var author = Mapper.Map<AuthorCreate>(authorCreate);
+        
     }
 }
