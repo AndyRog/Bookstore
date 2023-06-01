fc6fd498551930e1945b30d343b72e90ff07eb93
Andy Rog <andy.rog@t-online.de>
2023-06-01 11:25:16 +0200

    Neue record BookSale(long BookId, int Quantity );

---
+++ Bookstore.Application/Dtos/BookSale.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Dtos;
+
+public record BookDelivery(long BookId, int Quantity );
+
