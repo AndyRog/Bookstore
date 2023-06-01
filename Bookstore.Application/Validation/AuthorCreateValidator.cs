7abc998e49012b60601f0385464ed8d978f17461
Andy Rog <andy.rog@t-online.de>
2023-06-01 12:19:23 +0200

    Die Regeln: Vorname und Nachname string Länge(max. 30) und das Buch hat keine Title (darf nicht leer sein) implementiert

---
+++ Bookstore.Application/Validation/AuthorCreateValidator.cs
@@ -0,0 +1,20 @@
+﻿using Bookstore.Application.Dtos;
+using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Validation;
+
+public class AuthorCreateValidator : AbstractValidator<AuthorCreate>
+{
+    public AuthorCreateValidator()
+    {
+        //Die Regeln: Vorname und Nachname string Länge(max. 30) und das Buch hat keine Title (darf nicht leer sein) implementiert
+
+        RuleFor(authorCreate => authorCreate.Title).NotEmpty().MaximumLength(100);
+    }
+}
