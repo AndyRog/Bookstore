6b08cc84594e17fbc1159b1604d079b5831facab
Andy Rog <andy.rog@t-online.de>
2023-06-01 12:24:38 +0200

    Neue class AuthorUpdateValidator mit den Regeln: Vorname und Nachname string Länge(max. 50) und darf nicht leer sein

---
+++ Bookstore.Application/Validation/AuthorUpdateValidator.cs
@@ -0,0 +1,21 @@
+﻿using Bookstore.Application.Dtos;
+using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Validation;
+
+public class AuthorUpdateValidator : AbstractValidator<AuthorUpdate>
+{
+    public AuthorUpdateValidator()
+    {
+        //Die Regeln: Vorname und Nachname string Länge(max. 30) und das Buch hat keine Title (darf nicht leer sein) implementiert
+
+        RuleFor(authorUpdate => authorUpdate.FirstName).NotEmpty().MaximumLength(100);
+        RuleFor(authorUpdate => authorUpdate.LastName).NotEmpty().MaximumLength(100);
+    }
+}
