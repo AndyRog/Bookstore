3c131afcd0a748c6614780fa46aac44186805efc
Andy Rog <andy.rog@t-online.de>
2023-06-02 20:40:57 +0200

    Neu Method: async Task ProcessBookDelivery(BookDelivery bookDelivery)

--- Bookstore.Application/Services/BookDeliveryService.cs
+++ Bookstore.Application/Services/BookDeliveryService.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Bookstore.Applicatio.Exceptions;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bookstore.Application.Services
+namespace Bookstore.Application.Services;
+
+public class BookDeliveryService
 {
-    internal class BookDeliveryService
+    public IBookRepository BookRepository { get; }
+    public BookDeliveryValidator BookDeliveryValidator { get; }
+
+    public BookDeliveryService(IBookRepository bookRepository, BookDeliveryValidator bookDeliveryValidator)
     {
+        BookRepository = bookRepository;
+        BookDeliveryValidator = bookDeliveryValidator;
     }
+    public async Task ProcessBookDelivery(BookDelivery bookDelivery)
+    {
+        await BookDeliveryValidator.ValidateAndThrowAsync(bookDelivery);
+
+        Book? book = await BookRepository.GetBookById(bookDelivery.BookId);
+        if (book == null)
+        {
+            throw new BookNotFoundException();
+        }
+
+        book.Quantity += bookDelivery.Quantity;
+
+        await BookRepository.Update();
+    }
+
 }
