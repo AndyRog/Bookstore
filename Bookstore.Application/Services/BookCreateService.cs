e7665430dc55f929ee3b11c9491b8c528fae109d
Andy Rog <andy.rog@t-online.de>
2023-06-02 12:11:58 +0200

    Neue Method: async Task<long> CreateBook(BookCreate bookCreate) erstelllt

--- Bookstore.Application/Services/BookCreateService.cs
+++ Bookstore.Application/Services/BookCreateService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
 using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
 using Bookstore.Domain.Validation;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,17 +16,25 @@ namespace Bookstore.Application.Services
     public class BookCreateService
     {
         public IBookRepository BookRepository { get; }
+        public IAuthorRepository AuthorRepository { get; }
         public IMapper Mapper { get; }
         public BookCreateValidator BookCreateValidator { get; }
         public BookValidator BookValidator { get; }
 
-        public BookCreateService(IBookRepository bookRepository, IMapper mapper, BookCreateValidator bookCreateValidator, BookValidator bookValidator)
+        public BookCreateService(IBookRepository bookRepository, IAuthorRepository authorRepository, IMapper mapper, BookCreateValidator bookCreateValidator, BookValidator bookValidator)
         {
             BookRepository = bookRepository;
+            AuthorRepository = authorRepository;
             this.Mapper = mapper;
             BookCreateValidator = bookCreateValidator;
             BookValidator = bookValidator;
         }
+        public async Task<long> CreateBook(BookCreate bookCreate)
+        {
+            await BookCreateValidator.ValidateAndThrowAsync(bookCreate);
+            var book = Mapper.Map<Book>(bookCreate);
 
+            Author? author = await AuthorRepository.FindByAsync(bookCreate.AuthorId);
+        }
     }
 }
