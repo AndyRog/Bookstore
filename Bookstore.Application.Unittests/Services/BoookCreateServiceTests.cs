801f452bc4d12f46d3d96fcc470898a97a83ac98
Andy Rog <andy.rog@t-online.de>
2023-06-02 14:07:05 +0200

    Neu Test-Methoode: async Task Add_Book()

--- Bookstore.Application.Unittests/Services/BoookCreateServiceTests.cs
+++ Bookstore.Application.Unittests/Services/BoookCreateServiceTests.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using AutoMapper;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Services;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using Bookstore.Domain.Validation;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Bookstore.Application.Unittests.Services
 {
-    internal class BoookCreateServiceTests
+    public class BoookCreateServiceTests
     {
+        public IMapper Mapper { get; }
+        public BookCreateValidator BookCreateValidator { get; }
+        public BookValidator BookValidator{ get; }
+
+
+        public BoookCreateServiceTests() 
+        {
+            Mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+
+            BookCreateValidator = new BookCreateValidator();
+            BookValidator = new BookValidator();
+        }
+
+        [Fact]
+        public async Task Add_Book()
+        {
+            //Arrange
+            var bookCreate = new BookCreate("1234567891234", "Test",1,0);
+            var bookRepositoryMock = new Mock<IBookRepository>();
+            var authorRepositoryMock = new Mock<IAuthorRepository>();
+
+            authorRepositoryMock.Setup(mock => mock.GetAuthorById(1)).ReturnsAsync(new Author());
+            var bookCreateService = new BookCreateService(bookRepositoryMock.Object, authorRepositoryMock.Object, Mapper,BookCreateValidator,BookValidator);
+
+            //Act
+            await bookCreateService.CreateBook(bookCreate);
+
+            //Assert
+            bookRepositoryMock.Verify(mock => mock.AddBook(It.IsAny<Book>()), Times.Once);
+
+        }
     }
 }
