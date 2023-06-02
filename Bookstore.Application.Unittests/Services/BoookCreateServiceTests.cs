413a1c54b081e074a7ae2ce71486d571c4591c48
Andy Rog <andy.rog@t-online.de>
2023-06-02 14:18:06 +0200

    Test-Method: Author_Not_Found_Exception_Auhtor()

--- Bookstore.Application.Unittests/Services/BoookCreateServiceTests.cs
+++ Bookstore.Application.Unittests/Services/BoookCreateServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Bookstore.Application.Contracts;
 using Bookstore.Application.Dtos;
+using Bookstore.Application.Exceptions;
 using Bookstore.Application.Services;
 using Bookstore.Application.Validation;
 using Bookstore.Domain.Entities;
@@ -13,40 +14,59 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Bookstore.Application.Unittests.Services
+namespace Bookstore.Application.Unittests.Services;
+
+public class BoookCreateServiceTests
 {
-    public class BoookCreateServiceTests
+    public IMapper Mapper { get; }
+    public BookCreateValidator BookCreateValidator { get; }
+    public BookValidator BookValidator{ get; }
+
+
+    public BoookCreateServiceTests() 
     {
-        public IMapper Mapper { get; }
-        public BookCreateValidator BookCreateValidator { get; }
-        public BookValidator BookValidator{ get; }
+        Mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
 
+        BookCreateValidator = new BookCreateValidator();
+        BookValidator = new BookValidator();
+    }
 
-        public BoookCreateServiceTests() 
-        {
-            Mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+    [Fact]
+    public async Task Add_Book()
+    {
+        //Arrange
+        var bookCreate = new BookCreate("1234567891234", "Test",1,0);
+        var bookRepositoryMock = new Mock<IBookRepository>();
+        var authorRepositoryMock = new Mock<IAuthorRepository>();
 
-            BookCreateValidator = new BookCreateValidator();
-            BookValidator = new BookValidator();
-        }
+        authorRepositoryMock.Setup(mock => mock.GetAuthorById(1)).ReturnsAsync(new Author());
+        var bookCreateService = new BookCreateService(bookRepositoryMock.Object, authorRepositoryMock.Object, Mapper,BookCreateValidator,BookValidator);
 
-        [Fact]
-        public async Task Add_Book()
-        {
-            //Arrange
-            var bookCreate = new BookCreate("1234567891234", "Test",1,0);
-            var bookRepositoryMock = new Mock<IBookRepository>();
-            var authorRepositoryMock = new Mock<IAuthorRepository>();
+        //Act
+        await bookCreateService.CreateBook(bookCreate);
 
-            authorRepositoryMock.Setup(mock => mock.GetAuthorById(1)).ReturnsAsync(new Author());
-            var bookCreateService = new BookCreateService(bookRepositoryMock.Object, authorRepositoryMock.Object, Mapper,BookCreateValidator,BookValidator);
+        //Assert
+        bookRepositoryMock.Verify(mock => mock.AddBook(It.IsAny<Book>()), Times.Once);
 
-            //Act
-            await bookCreateService.CreateBook(bookCreate);
+    }
 
-            //Assert
-            bookRepositoryMock.Verify(mock => mock.AddBook(It.IsAny<Book>()), Times.Once);
+    [Fact]
+    public void Author_Not_Found_Exception_Auhtor()
+    {
+        //Arrange
+        var bookCreate = new BookCreate("1234567891234", "Test", 1, 0);
+        var bookRepositoryMock = new Mock<IBookRepository>();
+        var authorRepositoryMock = new Mock<IAuthorRepository>();
 
-        }
+        authorRepositoryMock.Setup(mock => mock.GetAuthorById(1)).Returns<Author?>(null);
+        var bookCreateService = new BookCreateService(bookRepositoryMock.Object, authorRepositoryMock.Object, Mapper, BookCreateValidator, BookValidator);
+
+        //Act
+        Func<Task> func = async () => await bookCreateService.CreateBook(bookCreate);
+
+        //Assert
+        Assert.ThrowsAsync<AuthorNotFoundException>(func);
+       
+
     }
 }
