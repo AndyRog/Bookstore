932fe89acd55351a96fdbd4013c852bb9bcb60a7
Andy Rog <andy.rog@t-online.de>
2023-06-02 19:57:08 +0200

    Neue Test-Method: async Task Update_Book()

--- Bookstore.Application.Unittests/Services/BookUpdateserviceTests.cs
+++ Bookstore.Application.Unittests/Services/BookUpdateserviceTests.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using AutoMapper;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using Bookstore.Domain.Validation;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Bookstore.Application.Unittests.Services
 {
-    internal class BookUpdateserviceTests
+    public class BookUpdateserviceTests
     {
+        private IMapper Mapper { get; }
+        private BookUpdateValidator BookUpdateValidator { get; }
+        private BookValidator BookValidator { get; }
+
+        public BookUpdateserviceTests()
+        {
+            Mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+
+            BookValidator = new BookValidator();
+            BookUpdateValidator = new BookUpdateValidator();
+        }
+
+        [Fact]
+        public async Task Update_Book()
+        {
+            //Arrange
+            var bookUpdate = new BookUpdate(1, "1234567891234", "Test", 1);
+
+            var bookRepositoryMock = new Mock<IBookRepository >();
+            bookRepositoryMock.Setup(mock => mock.GetBookId(1)).ReturnsAsync(new Book());
+
+            var authorRepositoryMock = new Mock<IAuthorRepository >();
+            authorRepositoryMock.Setup(mock => mock.GetAuthorById(1)).ReturnsAsync(new Author());
+
+            var bookUpdateService = new BookUpdateService(bookRepositoryMock.Object, authorRepositoryMock.Object, Mapper, BookValidator, BookUpdateValidator);
+
+            //Act
+            await bookUpdateService.UpdateBook(bookUpdate);
+
+            //Assert
+            bookRepositoryMock.Verify(mock => mock.Update(), Times.Once());
+
+        }
     }
 }
