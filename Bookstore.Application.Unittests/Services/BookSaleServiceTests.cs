979aa18b865eeda5ad88767efeb917fa9ec94bf7
Andy Rog <andy.rog@t-online.de>
2023-06-05 11:13:01 +0200

    Neue Test-Klasse: BookSaleServiceTests

---
+++ Bookstore.Application.Unittests/Services/BookSaleServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Bookstore.Applicatio.Exceptions;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Services;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using Bookstore.Domain.Validation;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bookstore.Application.Unittests.Services
+{
+    public class BookSaleServiceTests
+    {
+        private IMapper Mapper { get; }
+        private BookSaleValidator BookSaleValidator { get; }
+        private BookValidator BookValidator { get; }
+
+        public BookSaleServiceTests()
+        {
+            Mapper = new MapperConfiguration(cfg =>cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+           
+            BookSaleValidator = new BookSaleValidator();
+            
+            BookValidator = new BookValidator();
+                       
+        }
+
+        [Fact]
+        public async Task Quantaty_Updated()
+        {
+            //Arrenge
+            var bookSale = new BookSale(1, 1);
+            
+            var bookRepositoryMock = new Mock<IBookRepository>();
+            bookRepositoryMock.Setup(mock => mock.GetBookById(1)).ReturnsAsync(new Book() { Quantity = 1, Author = new Author()});
+            var bookSaleService = new BookSaleService( bookRepositoryMock.Object, BookSaleValidator, BookValidator);
+            //Act
+            await bookSaleService.ProcessBookDelivery(bookSale);
+
+            //Assert
+            bookRepositoryMock.Verify(mock => mock.Update(), Times.Once());
+        }
+
+        [Fact]
+        public void BookNotFoundException_For_Non_Existent_BookId()
+        {
+            //Arrenge
+            var bookSale = new BookSale(1, 1);
+
+            var bookRepositoryMock = new Mock<IBookRepository>();
+            bookRepositoryMock.Setup(mock => mock.GetBookById(1)).Returns<Book?>(null);
+            var bookSaleService = new BookSaleService(bookRepositoryMock.Object, BookSaleValidator, BookValidator);
+            //Act
+            Func<Task> func = async () => await bookSaleService.ProcessBookDelivery(bookSale);
+
+            //Assert
+           Assert.ThrowsAsync<BookNotFoundException>(func);
+
+        }
+    }
+}
---
+++ Bookstore.Application/Services/BookSaleService.cs
@@ -0,0 +1,47 @@
+﻿using Bookstore.Applicatio.Exceptions;
+using Bookstore.Application.Contracts;
+using Bookstore.Application.Dtos;
+using Bookstore.Application.Validation;
+using Bookstore.Domain.Entities;
+using Bookstore.Domain.Validation;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Services;
+
+public class BookSaleService
+{
+    public IBookRepository BookRepository { get; }
+    public BookSaleValidator BookSaleValidator { get; }
+    public BookValidator BookValidator { get; }
+
+    public BookSaleService(IBookRepository bookRepository, BookSaleValidator bookSaleValidator, BookValidator bookValidator)
+    {
+        BookRepository = bookRepository;
+        BookSaleValidator = bookSaleValidator;
+        BookValidator = bookValidator;
+    }
+   
+    
+    public async Task ProcessBookDelivery(BookSale bookSale)
+    {
+        await BookSaleValidator.ValidateAndThrowAsync(bookSale);
+
+        Book? book = await BookRepository.GetBookById(bookSale.BookId);
+        if (book == null)
+        {
+            throw new BookNotFoundException();
+        }
+
+        book.Quantity -= bookSale.Quantity;
+
+        await BookValidator.ValidateAndThrowAsync(book);
+
+        await BookRepository.Update();
+    }
+
+}
