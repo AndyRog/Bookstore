dcb7f85b4f59951c781ac5297be8e1b32e839688
Andy Rog <andy.rog@t-online.de>
2023-06-05 17:56:46 +0200

    Neue Klasse: AuthorRepository erstellt

---
+++ Bookstore.Ifrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,36 @@
+﻿using Bookstore.Application.Contracts;
+using Bookstore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Ifrastructure.Repositories
+{
+    internal class AuthorRepository : IAuthorRepository
+    {
+        public ApplicationDbContext DbContext { get; }
+        
+        public AuthorRepository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+
+        public Task<long> AddAuthorAsync(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Author?> GetAuthorByIdAsync(long authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
