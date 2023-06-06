b31d7d330bf2dedf079f2527e23894fb21924bc0
Andy Rog <andy.rog@t-online.de>
2023-06-06 18:20:18 +0200

    Internal class in public class umwandeln

--- Bookstore.Ifrastructure/IdentitySeed.cs
+++ Bookstore.Ifrastructure/IdentitySeed.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace Bookstore.Ifrastructure
 {
-    internal class IdentitySeed
+    public class IdentitySeed
     {
+        public UserManager<IdentityUser> UserManager { get; }
+        public RoleManager<IdentityRole> RoleManager { get; }
+        public ApplicationDbContext ApplicationDbContext { get; }
+        public IConfiguration Configuration { get; }
+
+        public IdentitySeed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext applicationDbContext, IConfiguration configuration)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+            ApplicationDbContext = applicationDbContext;
+            Configuration = configuration;
+        }
+
+        public async Task Seed()
+        {
+            var email = Configuration["ADMIN_EMAIL"];
+            var password = Configuration["ADMIN_PW"];
+
+            var roles = new string[] { "Admin", "User" };
+            foreach (var role in roles) 
+            {
+                if (!ApplicationDbContext.Roles.Any(r => r.Name == role))
+                {
+                    await RoleManager.CreateAsync(new IdentityRole(role));
+                }
+
+                if (!ApplicationDbContext.Users.Any(u => u.UserName == email))
+                {
+                    var identity = new IdentityUser
+                    {UserName = email, Email = email};
+
+                    var result = await UserManager.CreateAsync(identity, password);
+                    
+                    var user = await UserManager.FindByNameAsync(email);
+
+                    await UserManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+        }
     }
 }
