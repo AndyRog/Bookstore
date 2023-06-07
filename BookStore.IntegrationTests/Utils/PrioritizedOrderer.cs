dcfe03ebf95f92253810e90d788a3133c82b32c0
Andy Rog <andy.rog@t-online.de>
2023-06-07 16:25:28 +0200

    Parallelization ausgeschaltet.

--- BookStore.IntegrationTests/Utils/PrioritizedOrderer.cs
+++ BookStore.IntegrationTests/Utils/PrioritizedOrderer.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+[assembly:CollectionBehavior(DisableTestParallelization = true)]
 namespace BookStore.IntegrationTests.Utils
 {
-    internal class PrioritizedOrderer
+    public class PrioritizedOrderer
     {
     }
 }
