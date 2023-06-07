c3f374cbf4c66072832e8af9b377b1971b4c1020
Andy Rog <andy.rog@t-online.de>
2023-06-07 21:14:10 +0200

    Update TestPriorityAttribute

--- BookStore.IntegrationTests/Utils/TestPriorityAttribute.cs
+++ BookStore.IntegrationTests/Utils/TestPriorityAttribute.cs
@@ -2,7 +2,7 @@
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class TestPriorityAttribute :Attribute
 {
-    public int Priority { get; }
+    protected int Priority { get; }
     public TestPriorityAttribute(int priority)
     {
         Priority = priority;
