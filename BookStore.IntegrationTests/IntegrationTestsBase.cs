28f52f6c584a337fcae91df26736c67ecf1547dc
Andy Rog <andy.rog@t-online.de>
2023-06-07 16:28:12 +0200

    Vererbung von Interface: "ITestCaseStarting" realesiert.

--- BookStore.IntegrationTests/IntegrationTestsBase.cs
+++ BookStore.IntegrationTests/IntegrationTestsBase.cs
@@ -4,42 +4,41 @@ using Bookstore.Ifrastructure;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BookStore.IntegrationTests
+namespace BookStore.IntegrationTests;
+
+public class IntegrationTestsBase : IClassFixture<WebApplicationFactory<Startup>>
 {
-    public class IntegrationTestsBase : IClassFixture<WebApplicationFactory<Startup>>
+
+
+    protected WebApplicationFactory<Startup> Factory { get; }
+    protected HttpClient Client { get; }
+    protected ApplicationDbContext DbContext { get; }
+    protected IMapper Mapper { get; }
+
+
+
+    public IntegrationTestsBase(WebApplicationFactory<Startup> factory) 
+    {
+        Environment.SetEnvironmentVariable("ADMIN_EMAIL", "admin@test.de");
+       
+        Environment.SetEnvironmentVariable("ADMIN_RW", "Admin!123Admin?");
+        
+        Factory = factory;
+        Client = factory.CreateClient();
+
+        var scopeFactory = factory.Services.GetService<IServiceScopeFactory>() ?? throw new Exception("Scope factory not found");
+        var scope = scopeFactory.CreateScope() ?? throw new Exception("Could not create Scope");
+
+        DbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>() ?? throw new Exception("cpold not get Application DbContext");
+
+        Mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
+
+    }
+
+
+    [Fact]
+    public void Test1()
     {
 
-
-        protected WebApplicationFactory<Startup> Factory { get; }
-        protected HttpClient Client { get; }
-        protected ApplicationDbContext DbContext { get; }
-        protected IMapper Mapper { get; }
-
-
-
-        public IntegrationTestsBase(WebApplicationFactory<Startup> factory) 
-        {
-            Environment.SetEnvironmentVariable("ADMIN_EMAIL", "admin@test.de");
-           
-            Environment.SetEnvironmentVariable("ADMIN_RW", "Admin!123Admin?");
-            
-            Factory = factory;
-            Client = factory.CreateClient();
-
-            var scopeFactory = factory.Services.GetService<IServiceScopeFactory>() ?? throw new Exception("Scope factory not found");
-            var scope = scopeFactory.CreateScope() ?? throw new Exception("Could not create Scope");
-
-            DbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>() ?? throw new Exception("cpold not get Application DbContext");
-
-            Mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(DtoEntityMapperProfile))).CreateMapper();
-
-        }
-
-
-        [Fact]
-        public void Test1()
-        {
-
-        }
     }
 }
--- BookStore.IntegrationTests/Utils/PrioritizedOrderer.cs
+++ BookStore.IntegrationTests/Utils/PrioritizedOrderer.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
+
 [assembly:CollectionBehavior(DisableTestParallelization = true)]
-namespace BookStore.IntegrationTests.Utils
+namespace BookStore.IntegrationTests.Utils;
+
+public class PrioritizedOrderer : ITestCaseStarting
 {
-    public class PrioritizedOrderer
-    {
-    }
+    public ITestCase TestCase => throw new NotImplementedException();
+
+    public ITestMethod TestMethod => throw new NotImplementedException();
+
+    public ITestClass TestClass => throw new NotImplementedException();
+
+    public ITestCollection TestCollection => throw new NotImplementedException();
+
+    public ITestAssembly TestAssembly => throw new NotImplementedException();
+
+    public IEnumerable<ITestCase> TestCases => throw new NotImplementedException();
 }
