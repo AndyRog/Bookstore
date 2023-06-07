3faee1599ba1d4f2c90c25f00f1628c62f170f3a
Andy Rog <andy.rog@t-online.de>
2023-06-07 12:36:45 +0200

    Klasse:"AuthorController" erstellt

---
+++ Bookstore/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using Bookstore.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : Controller
+    {
+        public AuthorCreateService AuthorCreateService { get; }
+        public AuthorUpdateService AuthorUpdateService { get; }
+
+        public AuthorController(AuthorCreateService authorCreateService, AuthorUpdateService authorUpdateService)
+        {
+            AuthorCreateService = authorCreateService;
+            AuthorUpdateService = authorUpdateService;
+        }
+
+
+
+    }
+}
