cac1158ca4d4484f45f447e09670b770eb90aab5
Andy Rog <andy.rog@t-online.de>
2023-06-08 21:03:27 +0200

    Test-Methode: "Success_Status_For_Created_Author()"  erstellt

--- BookStore.IntegrationTests/AuthorCreateTests.cs
+++ BookStore.IntegrationTests/AuthorCreateTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Bookstore.Application.Dtos;
+using Bookstore.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +16,34 @@ public class AuthorCreateTests : IntegrationTestsBase
     public AuthorCreateTests(WebApplicationFactory<Startup> factory) : base(factory)
     {
         
+    }
+
+    [Fact]
+    public async Task Success_Status_For_Created_Author()
+    {
+        //Arrange
+        var authorCreate = new AuthorCreate("Test", "Test");
+
+        var authorCreateJson = JsonConvert.SerializeObject(authorCreate);
+
+        var content = new StringContent(authorCreateJson, Encoding.UTF8,"application/json");
+
+        var expectedAuthor = Mapper.Map<Author>(authorCreate);
+
+
+        //Act
+        var response = await Client.PostAsync("/Autor/Create", content);
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var authorenId = int.Parse(responseContent);
+
+        //Get Author from DB
+        var authorInDb = await DbContext.Authors.Where(author => author.Id == authorenId).SingleAsync();
+
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(expectedAuthor, authorInDb)
+
     }
 }
