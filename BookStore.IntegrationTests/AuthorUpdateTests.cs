41d6ce80cb733130c4f17d6eeb3747ba7d449431
Andy Rog <andy.rog@t-online.de>
2023-06-09 21:02:20 +0200

    Test-Methode: "Erfolgsstatuscode für aktualisierten Autor" erstellt

--- BookStore.IntegrationTests/AuthorUpdateTests.cs
+++ BookStore.IntegrationTests/AuthorUpdateTests.cs
@@ -1,5 +1,7 @@
-﻿using Bookstore.Domain.Entities;
+﻿using Bookstore.Application.Dtos;
+using Bookstore.Domain.Entities;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,5 +26,24 @@ public class AuthorUpdateTests : IntegrationTestsBase
         DbContext.SaveChanges();
     }
 
+    [Fact]
+    public async Task Success_StatusCode_For_Updated_Autor()
+    {
+        //Arrange
+        var authorUpdate = new AuthorUpdate(Author.Id, "Test", "Test2");
+        var authorUpdateJson = JsonConvert.SerializeObject(authorUpdate);
+        var content = new StringContent(authorUpdateJson, Encoding.UTF8, "application/json");
+        var expectedAuthor = Mapper.Map<Author>(authorUpdate);
 
+        //Act
+        var response = await Client.PutAsync("Author/Update", content);
+
+        //Get Author from DB
+        await DbContext.Entry(Author).ReloadAsync();
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(expectedAuthor, Author);
+
+    }
 }
