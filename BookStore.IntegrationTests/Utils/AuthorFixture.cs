33b3618f4f33387f11e0a1977ff8e1685f0943cb
Andy Rog <andy.rog@t-online.de>
2023-06-10 23:23:16 +0200

    Autorenfixierung

---
+++ BookStore.IntegrationTests/Utils/AuthorFixture.cs
@@ -0,0 +1,23 @@
+﻿using Bookstore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.IntegrationTests.Utils
+{
+    public class AuthorFixture
+    {
+        public Author Author { get; }
+        public AuthorFixture()
+        {
+            Author = new Author()
+            {
+                FirstName = "Test",
+                LastName = "Test"
+            };
+        }
+
+    }
+}
