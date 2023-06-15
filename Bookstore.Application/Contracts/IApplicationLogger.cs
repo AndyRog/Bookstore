7cdf260631b8910bc81b394aadf71551fab272a8
Andy Rog <andy.rog@t-online.de>
2023-06-15 12:01:04 +0200

    Methoden in Interface:"  IApplicationLogger" erstellt.

--- Bookstore.Application/Contracts/IApplicationLogger.cs
+++ Bookstore.Application/Contracts/IApplicationLogger.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bookstore.Application.Dtos;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace Bookstore.Application.Contracts
 {
-    internal interface IApplicationLogger
+    public interface IApplicationLogger<T>
     {
+        void LogAuthorCreated(long id);
+        void LogCreateAuthorAsyncCalled(AuthorCreate authorCreate);
+        void LogValidationErrorInCreateAuthor(ValidationException ex, AuthorCreate authorCreate);
     }
 }
