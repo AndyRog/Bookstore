051b9df116c53b2dc57b23197bd7084b006affef
Andy Rog <andy.rog@t-online.de>
2023-06-20 13:08:32 +0200

    Interface IApplication Methoden implementiert

--- Bookstore.Application/ApplicationLogger.cs
+++ Bookstore.Application/ApplicationLogger.cs
@@ -32,13 +32,13 @@ namespace Bookstore.Application
 
         public void LogValidationErrorForCreateAuthor(ValidationException ex, AuthorCreate authorCreate)
         {
-            Logger.LogError(ex, "Validation Error in CreateAuthor. {authorCreate}", authorCreate);
+            Logger.LogError(ex, "Validation Error for CreateAuthor. {authorCreate}", authorCreate);
 
         }
 
         public void LogAuthorNotFound(long authorId)
         {
-           Logger.LogError($"Author not found in UpdateAuthor. {authorId}");
+           Logger.LogError($"Author not found. {authorId}");
         }
 
         public void LogAuthorUpdated(Author author)
@@ -53,7 +53,83 @@ namespace Bookstore.Application
 
         public void LogValidationErrorForAuthorUpdate(ValidationException ex, AuthorUpdate authorUpdate)
         {
-            Logger.LogError(ex, "Validation Error in UpdateAuthor. {authorUpdate}", authorUpdate);
+            Logger.LogError(ex, "Validation Error for AuthorUpdate. {authorUpdate}", authorUpdate);
+        }
+
+        public void LogCreateBookAsyncCalled(BookCreate bookCreate)
+        {
+            Logger.LogInformation($"CreateBook called. {bookCreate}");
+        }
+
+        public void LogValidationErrorForBookCreate(ValidationException ex, BookCreate bookCreate)
+        {
+            Logger.LogError(ex, $"Validation Error for BookCreate. {bookCreate}");
+        }
+
+        public void LogIsbnDuplicate(string isbn)
+        {
+          Logger.LogInformation($"ISBN duplicate{isbn}");
+        }
+
+        public void LogBookCreated(long id)
+        {
+            Logger.LogInformation($"Book created. {id}");
+        }
+
+        public void LogValidationErrorForBook(ValidationException ex, Book book)
+        {
+            Logger.LogError(ex,$"Validation Error for book. {book}");
+           
+        }
+
+        public void LogProcessBookDeliveryAsyncCalled(BookDelivery bookDelivery)
+        {
+            Logger.LogInformation($"ProcessBookDelivery called. {bookDelivery}");
+        }
+
+        public void LogUpdateBookCalled(BookUpdate bookUpdate)
+        {
+            Logger.LogInformation($"UpdateBook called. {bookUpdate}");
+        }
+
+        public void LogProcessBookSaleAsyncCalled(BookSale bookSale)
+        {
+            Logger.LogInformation($"ProcessBookSale called. {bookSale}");
+        }
+
+        public void LogValidationErrorForBookDelivery(ValidationException ex, BookDelivery bookDelivery)
+        {
+            Logger.LogError(ex, $"Validation Error for BookDelivery. {bookDelivery}");
+        }
+
+        public void LogValidationErrorForBookUpdate(ValidationException ex, BookUpdate bookUpdate)
+        {
+            Logger.LogError(ex, $"Validation Error for BookUpdate. {bookUpdate}");
+        }
+
+        public void LogBookNotFound(long bookId)
+        {
+           Logger.LogError($"Book not found. {bookId}");
+        }
+
+        public void LogValidationErrorForBookSale(ValidationException ex, BookSale bookSale)
+        {
+            Logger.LogError(ex, $"Validation Error for BookSale {bookSale}");
+        }
+
+        public void LogBookUpdated(Book book)
+        {
+            Logger.LogInformation($"Book updated. {book}");
+        }
+
+        public void LogBookSaleProcessed(long id, int quantity)
+        {
+            Logger.LogInformation($"BookSale processed. {id} {quantity}");
+        }
+
+        public void LogBookDeliveryProcessed(long id, int quantity)
+        {
+            Logger.LogInformation($"BookDelivery processed. {id} {quantity}");
         }
     }
 }
