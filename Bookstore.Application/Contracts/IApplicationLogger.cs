de107f87db4e241202e7c21308ac598b1c079ee1
Andy Rog <andy.rog@t-online.de>
2023-06-20 12:20:39 +0200

    Interface Methoden für Book deklariert

--- Bookstore.Application/Contracts/IApplicationLogger.cs
+++ Bookstore.Application/Contracts/IApplicationLogger.cs
@@ -11,12 +11,36 @@ namespace Bookstore.Application.Contracts
 {
     public interface IApplicationLogger<T>
     {
-        void LogAuthorNotFound(long authorId);
-        void LogAuthorCreated(long id);
-        void LogAuthorUpdated(Author author);
+        //Author
         void LogCreateAuthorAsyncCalled(AuthorCreate authorCreate);
+        void LogValidationErrorForCreateAuthor(ValidationException ex, AuthorCreate authorCreate);
+        void LogAuthorCreated(long id);
         void LogUpdateAuthorAsyncCalled(AuthorUpdate authorUpdate);
-        void LogValidationErrorInCreateAuthor(ValidationException ex, AuthorCreate authorCreate);
-        void LogValidationErrorInUpdateAuthor(ValidationException ex, AuthorUpdate authorUpdate);
+        void LogValidationErrorForAuthorUpdate(ValidationException ex, AuthorUpdate authorUpdate);
+        void LogAuthorNotFound(long authorId);
+        void LogAuthorUpdated(Author author);
+      
+        //Book
+        void LogCreateBookAsyncCalled(BookCreate bookCreate);
+        void LogValidationErrorForBookCreate(ValidationException ex, BookCreate bookCreate);
+        void LogIsbnDuplicate(string isbn);
+        void LogBookCreated(long id);
+        void LogProcessBookDeliveryAsyncCalled(BookDelivery bookDelivery);
+        void LogValidationErrorForBook(ValidationException ex, Book book);
+        void LogUpdateBookCalled(BookUpdate bookUpdate);
+        void LogProcessBookSaleAsyncCalled(BookSale bookSale);
+        void LogValidationErrorForBookDelivery(ValidationException ex,BookDelivery bookDelivery);
+        void LogValidationErrorForBookUpdate(ValidationException ex,BookUpdate bookUpdate);        
+        void LogBookNotFound(long bookId);
+        void LogValidationErrorForBookSale(ValidationException ex, BookSale bookSale);
+        void LogBookUpdated(Book book);
+        void LogBookSaleProcessed(long id, int quantity);
+        void LogBookDeliveryProcessed(long id, int quantity);
+        
+
+       
+        
+       
+
     }
 }
